8c62fc1d27a8a97c2d081ced3a41b68819a5c3b2
OrGutman <or.gutman7@gmail.com>
2025-11-13 13:18:11 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,4 +3,21 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    list = []
+    letters = ""
+    numbers = ""
+
+    for i in formula:
+        is_letter = i.isdigit()
+        if not is_letter:
+            letters += i
+        else:
+            numbers += i
+    list.append(letters)
+
+    if numbers != "":
+        list.append(int(numbers))
+    else:
+        list.append(1)
+
+    return list
